e2523098265d13545a98b9e3727c97ff9afe8a1e
Aurélien Perrier <a.perrier89@gmail.com>
2022-02-06 20:18:29 +0100

    Fix filters dates quotes

--- proto/positions/v1/position.pb.validate.go
+++ proto/positions/v1/position.pb.validate.go
@@ -35,6 +35,141 @@ var (
 	_ = sort.Sort
 )
 
+// Validate checks the field values on Date with the rules defined in the proto
+// definition for this message. If any rules are violated, the first error
+// encountered is returned, or nil if there are no violations.
+func (m *Date) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on Date with the rules defined in the
+// proto definition for this message. If any rules are violated, the result is
+// a list of violation errors wrapped in DateMultiError, or nil if none found.
+func (m *Date) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *Date) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if _, ok := Date_Interval_name[int32(m.GetInterval())]; !ok {
+		err := DateValidationError{
+			field:  "Interval",
+			reason: "value must be one of the defined enum values",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if _, ok := _Date_Period_InLookup[m.GetPeriod()]; !ok {
+		err := DateValidationError{
+			field:  "Period",
+			reason: "value must be in list [last 3d 1w 2w 1m 2m 3m 6m 1y 2y 3y 5y]",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if len(errors) > 0 {
+		return DateMultiError(errors)
+	}
+
+	return nil
+}
+
+// DateMultiError is an error wrapping multiple validation errors returned by
+// Date.ValidateAll() if the designated constraints aren't met.
+type DateMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m DateMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m DateMultiError) AllErrors() []error { return m }
+
+// DateValidationError is the validation error returned by Date.Validate if the
+// designated constraints aren't met.
+type DateValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e DateValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e DateValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e DateValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e DateValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e DateValidationError) ErrorName() string { return "DateValidationError" }
+
+// Error satisfies the builtin error interface
+func (e DateValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sDate.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = DateValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = DateValidationError{}
+
+var _Date_Period_InLookup = map[string]struct{}{
+	"last": {},
+	"3d":   {},
+	"1w":   {},
+	"2w":   {},
+	"1m":   {},
+	"2m":   {},
+	"3m":   {},
+	"6m":   {},
+	"1y":   {},
+	"2y":   {},
+	"3y":   {},
+	"5y":   {},
+}
+
 // Validate checks the field values on PositionRequest with the rules defined
 // in the proto definition for this message. If any rules are violated, the
 // first error encountered is returned, or nil if there are no violations.
@@ -57,17 +192,6 @@ func (m *PositionRequest) validate(all bool) error {
 
 	var errors []error
 
-	if _, ok := Broker_name[int32(m.GetBroker())]; !ok {
-		err := PositionRequestValidationError{
-			field:  "Broker",
-			reason: "value must be one of the defined enum values",
-		}
-		if !all {
-			return err
-		}
-		errors = append(errors, err)
-	}
-
 	if l := utf8.RuneCountInString(m.GetAccount()); l < 3 || l > 15 {
 		err := PositionRequestValidationError{
 			field:  "Account",
@@ -79,15 +203,44 @@ func (m *PositionRequest) validate(all bool) error {
 		errors = append(errors, err)
 	}
 
-	if _, ok := _PositionRequest_Interval_InLookup[m.GetInterval()]; !ok {
+	if m.GetDate() == nil {
 		err := PositionRequestValidationError{
-			field:  "Interval",
-			reason: "value must be in list [last 3d 1w 2w 1m 3m 6m 1y 3y 5y 10y]",
+			field:  "Date",
+			reason: "value is required",
 		}
 		if !all {
 			return err
 		}
 		errors = append(errors, err)
+	}
+
+	if all {
+		switch v := interface{}(m.GetDate()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, PositionRequestValidationError{
+					field:  "Date",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, PositionRequestValidationError{
+					field:  "Date",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetDate()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return PositionRequestValidationError{
+				field:  "Date",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
 	}
 
 	if len(errors) > 0 {
@@ -168,20 +321,6 @@ var _ interface {
 	ErrorName() string
 } = PositionRequestValidationError{}
 
-var _PositionRequest_Interval_InLookup = map[string]struct{}{
-	"last": {},
-	"3d":   {},
-	"1w":   {},
-	"2w":   {},
-	"1m":   {},
-	"3m":   {},
-	"6m":   {},
-	"1y":   {},
-	"3y":   {},
-	"5y":   {},
-	"10y":  {},
-}
-
 // Validate checks the field values on PositionCompanyReply with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
@@ -204,9 +343,9 @@ func (m *PositionCompanyReply) validate(all bool) error {
 
 	var errors []error
 
-	// no validation rules for Conid
+	// no validation rules for Ticker
 
-	// no validation rules for Currency
+	// no validation rules for Exchange
 
 	// no validation rules for Size
 
@@ -355,8 +494,6 @@ func (m *PositionCompanyReplies) validate(all bool) error {
 	}
 
 	// no validation rules for Total
-
-	// no validation rules for Date
 
 	if len(errors) > 0 {
 		return PositionCompanyRepliesMultiError(errors)
@@ -603,8 +740,6 @@ func (m *PositionCurrencyReplies) validate(all bool) error {
 	}
 
 	// no validation rules for Total
-
-	// no validation rules for Date
 
 	if len(errors) > 0 {
 		return PositionCurrencyRepliesMultiError(errors)
