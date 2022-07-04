604571dd79630d304dd16e381c3325942f996691
Aurélien Perrier <a.perrier89@gmail.com>
2022-07-04 17:49:03 +0200

    feat: companies strategies results

--- proto/companies/v1/companies.pb.validate.go
+++ proto/companies/v1/companies.pb.validate.go
@@ -1321,6 +1321,52 @@ func (m *CompanyReply) validate(all bool) error {
 		}
 	}
 
+	{
+		sorted_keys := make([]string, len(m.GetStrategies()))
+		i := 0
+		for key := range m.GetStrategies() {
+			sorted_keys[i] = key
+			i++
+		}
+		sort.Slice(sorted_keys, func(i, j int) bool { return sorted_keys[i] < sorted_keys[j] })
+		for _, key := range sorted_keys {
+			val := m.GetStrategies()[key]
+			_ = val
+
+			// no validation rules for Strategies[key]
+
+			if all {
+				switch v := interface{}(val).(type) {
+				case interface{ ValidateAll() error }:
+					if err := v.ValidateAll(); err != nil {
+						errors = append(errors, CompanyReplyValidationError{
+							field:  fmt.Sprintf("Strategies[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				case interface{ Validate() error }:
+					if err := v.Validate(); err != nil {
+						errors = append(errors, CompanyReplyValidationError{
+							field:  fmt.Sprintf("Strategies[%v]", key),
+							reason: "embedded message failed validation",
+							cause:  err,
+						})
+					}
+				}
+			} else if v, ok := interface{}(val).(interface{ Validate() error }); ok {
+				if err := v.Validate(); err != nil {
+					return CompanyReplyValidationError{
+						field:  fmt.Sprintf("Strategies[%v]", key),
+						reason: "embedded message failed validation",
+						cause:  err,
+					}
+				}
+			}
+
+		}
+	}
+
 	// no validation rules for Employees
 
 	// no validation rules for CurrencyReport
